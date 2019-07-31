48561f90c43ad3514d7d1d29b92e02d083d13e86
bengisucam <bengisu.cam@gmail.com>
2019-07-31 12:02:03 +0300

    second commit

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload to .
+          Edit <code>src/App.js</code> and save to reload to end.
         </p>
         <a
           className="App-link"
