53ad4fc074a639a9d009cd3727c59805e030809d
bengisucam <bengisu.cam@gmail.com>
2019-07-31 15:40:03 +0300

    second commit

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import MyComponents from './components/MyComponents';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload to end.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <MyComponents name="bengisucam"></MyComponents>
+     <MyComponents name = "bilgecam"></MyComponents>
+     <MyComponents name = "bilgeecam" This is my > children </MyComponents>
+     <MyComponents name = "bilgeeecam" This is my > children </MyComponents>
+
     </div>
   );
 }
---
+++ my-first-react-app/src/components/MyComponents.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import './MyComponents.css';
+
+
+class MyComponents extends Component {
+
+    state = {
+        count: 0
+        //num: "1"
+    };
+
+    increaseCount = () => {
+        console.log("increased");
+        console.log(this.state.count);
+        this.setState({count: this.state.count + 1});
+
+    };
+
+    divStyle = {
+        fontSize:"50px"
+    }
+
+    render(){
+
+        let myVar = "my var"
+
+        return <div className = "MyClass" style = {{fontSize: this.state.count + "px"}}  >This is my component
+                    And my name is {this.props.name}
+                     <br/> also my variable is {myVar}
+                     <br/> and my child is {this.props.children}
+                     <br/> and my count is {this.state.count}
+                     <br/> and my num is {this.state.num}
+                     <br/>
+                        <button onClick={ () => this.increaseCount() } >increase count </button>   </div>;
+      }
+}
+
+
+export default MyComponents;
